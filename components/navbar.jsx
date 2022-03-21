08d183cfc15403c19630a5391a77d9ac514d5d07
Nigel van Duijvendijk <55701170+NigelvanDuijvendijk@users.noreply.github.com>
2022-03-21 11:10:02 +0100

    changed class to className

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -2,21 +2,21 @@ import Link from 'next/link';
 
 export const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <Link href="/"><a class="navbar-brand">Navbar</a></Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link href="/"><a className="navbar-brand">Navbar</a></Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
                     <Link href="/projecten">
-                        <a class="nav-item nav-link">Projecten</a>
+                        <a className="nav-item nav-link">Projecten</a>
                     </Link>
                     <Link href="/technologieen">
-                        <a class="nav-item nav-link">Technologieen</a>
+                        <a className="nav-item nav-link">Technologieen</a>
                     </Link>
                     <Link href="/about">
-                        <a class="nav-item nav-link">About</a>
+                        <a className="nav-item nav-link">About</a>
                     </Link> 
                 </div>
             </div>
